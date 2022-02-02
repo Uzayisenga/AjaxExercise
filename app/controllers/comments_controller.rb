8aedaa95e9f29f737110f16aa1df6e22afe2300d
sefty <seftyyunitasari@gmail.com>
2022-02-02 17:21:02 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+        #format.html { redirect_to blog_path(@blog) }
+      else
+        format.html {redirect_to blog_path(@blog), notice: 'could not post comment'}
+      end
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
+  end
+end
